b9f59f511c8ddb257e5c9b262a23803c9e93497f
SVC20000 <svc2000@hw.ac.uk>
2023-11-09 20:16:52 +0400

    buidqbkja

--- week4/app.js
+++ week4/app.js
@@ -1,35 +1,32 @@
+const api = '19ea4c52afce7276e8b4bd05c1971df2';
 const input = document.getElementById("cityInput");
 const button = document.getElementById("btn");
-const info = document.getElementById("weather-info");
+const weather_info = document.getElementById("weather-info");
 
-button. addEventListener('click', function(){
-  const api = '19ea4c52afce7276e8b4bd05c1971df2';
-  const cityInput = input.value;
+button. addEventListener("click", function(){
 
-  if (!cityInput){
-    alert ("Please Enter a City name"); 
+  const city = input.value;
+
+  if (!city){
+    alert ("Please Enter a City Name."); 
     return;
   }
 
   var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET',`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${api}`);
+  ourRequest.open('GET',`https://api.openweathermap.org/data/2.5/weather?q=` + city + `&units=metric&appid=` + api);
 
   ourRequest.onload = function() {
     if (ourRequest. status >= 200 && ourRequest.status < 400) {
-    var data = JSON.parse (ourRequest.responseText);
-    if(data.cod === 200){
-      renderWeather(data);
+    var ourData = JSON.parse(ourRequest.responseText);
+    if(ourData.cod === 200){
+      renderWeather(ourData);
+    } else {
+    alert("City not found");
     }
-    
-  } else {
-    console.error('HTTP Status Code Error:', ourRequest.status);
+} else {
+    alert("Error!")
   }
-};
-ourRequest.onreadystatechange = function() {
-  if (ourRequest.readyState == 4 && ourRequest.status == 200) {
-  const errorResponse = JSON.parse(ourRequest.responseText);
-  console.error('API-Specific Error:', errorResponse.message);
-  }
+
 };
 ourRequest.onerror = function() {
   console.error('Network Error');
@@ -39,8 +36,8 @@ ourRequest.send();
 
 function renderWeather (Data){
   var temp = (Data.main.temp - 273.15).toFixed(2);
-  var HTMLtext = "<p> The weather in " + Data.name + "is" + Data.weather[0].description + ".<br> The temperature is " + temp + "°C with a wind speed of " + Data.wind.speed + "m/s. <hr></p>";
+  var HTMLtext = "<p> The weather in " + Data.name + " is " + Data.weather[0].description + ".<br> The temperature is " + Data.main.temp + "°C with a wind speed of " + Data.wind.speed + "m/s. <hr></p>";
 
-  info.insertAdjacentHTML('beforeend', HTMLtext);
+  weather_info.insertAdjacentHTML('beforeend', HTMLtext);
 }
   
