dc11578386a9dc14707c025be1b5f173ec54b453
SVC20000 <svc2000@hw.ac.uk>
2023-11-09 13:56:26 +0400

    ygvqieba

--- week4/main.js
+++ week4/main.js
@@ -2,7 +2,7 @@ var cityContainer = document.getElementById("city-info");
 var btn = document.getElementById("btn");
 btn.addEventListener("click", function(){
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://svc20000.github.io/F28WP/cities1.json');
+ourRequest.open('GET', 'https://svc20000.github.io/F28WP/week4/cities1.json');
 ourRequest.onload = function() {
 var ourData = JSON.parse(ourRequest.responseText);
 renderHTML(ourData);
